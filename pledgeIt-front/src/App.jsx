29129b0af54a2b1d198f694f3ad3fce0befa3f5c
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2024-12-19 00:58:48 +0530

    About Page created
    I created an about page.. but some errors are found

--- pledgeIt-front/src/App.jsx
+++ pledgeIt-front/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import VolunteerCategories from './components/VolunteerCategories';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -8,5 +10,15 @@ const App = () => {
     </div>
   );
 };
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
